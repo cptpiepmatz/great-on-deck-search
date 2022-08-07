633c4456d93927e0221ef631f464fc18f91b7127
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-08-07 11:40:10 +0200

    added persistence for the settings

---
+++ src/settings/settings.js
@@ -0,0 +1,36 @@
+/** IDs of the toggles, will also be used as keys for the settings. */
+const toggleIds = ["toggleProtonDB", "toggleDeckVerified", "toggleSteamDeckHQ"];
+
+/**
+ * Fetches the settings from the browser.
+ * @param keys Keys to fetch settings
+ * @return {Promise<object>}
+ */
+function fetchSettings(...keys) {
+  return new Promise(resolve => {
+    chrome.storage.sync.get(keys, resolve);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  // fetches all the settings and defaults to true for every key
+  let settings = Object.assign(
+    Object.fromEntries(toggleIds.map(id => [id, true])),
+    await fetchSettings(...toggleIds)
+  );
+
+  // get every toggle and update it value to the settings
+  let toggles = new Map(toggleIds.map(id => [id, document.getElementById(id)]));
+  for (let [id, toggle] of toggles) {
+    toggle.checked = settings[id];
+    toggle.addEventListener("click", e => {
+      // upon clicking on a toggle it should update the setting
+      e.preventDefault();
+      let key = {};
+      key[id] = toggle.checked;
+      chrome.storage.sync.set(key, res => {
+        toggle.checked = !toggle.checked;
+      });
+    });
+  }
+});
