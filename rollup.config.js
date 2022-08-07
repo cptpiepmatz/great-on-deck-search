2ce385fa4d02b5aecb6bc15bd2fb1f985586c27c
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-08-07 23:58:20 +0200

    added rollup for bundling

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    input: "src/pages/index.js",
+    output: {
+      file: "out/pages.bundle.js",
+      format: "iife"
+    }
+  },
+  {
+    input: "src/background/index.js",
+    output: {
+      file: "out/background.bundle.js",
+      format: "iife"
+    }
+  }
+]
