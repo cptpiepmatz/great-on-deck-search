8b82434e42dec573a4af232569549c426793df20
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-08-09 15:38:43 +0200

    rollup now also emits sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,16 @@ export default [
     input: "src/pages/index.js",
     output: {
       file: "out/pages.bundle.js",
-      format: "iife"
+      format: "iife",
+      sourcemap: "inline"
     }
   },
   {
     input: "src/background/index.js",
     output: {
       file: "out/background.bundle.js",
-      format: "iife"
+      format: "iife",
+      sourcemap: "inline"
     }
   }
 ]
